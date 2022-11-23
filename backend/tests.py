161381cfe981891eec4e3316b27be1b5d03e6e0d
Golovnya Vitaliy <vitas210295@gmail.com>
2022-11-23 22:06:29 +1000

    APITestCase authentification solution found

--- backend/tests.py
+++ backend/tests.py
@@ -1,21 +1,11 @@
 from django.urls import reverse
 from rest_framework import status
 from rest_framework.test import APITestCase
-from django.test import Client
+
 from backend.models import Order
-from backend.serializers import CustomUserSerializer
 from users.models import CustomUser
 
-
-class CustomUserTests(APITestCase):
-    '''
-    test user creation
-    '''
-
-    def test_signup(self):
-        c = Client()
-        url = reverse('signup')
-        data = {'email': 'test@test.com',
+user_data = {'email': 'test@test.com',
                 'password': 'test_password',
                 'password2': 'test_password',
                 'company': 'test_company',
@@ -24,31 +14,35 @@ class CustomUserTests(APITestCase):
                 'name': 'test_name',
                 'surname': 'test_surname',
                 'middle_name': 'test_middle_name'}
-        response = c.post(url, data, format='json')
+
+user_url = reverse('signup')
+
+class CustomUserTests(APITestCase):
+    '''
+    test user creation
+    '''
+
+    def test_signup_login(self):
+        response = self.client.post(user_url, user_data, format='json')
+        self.assertTrue(self.client.login(email='test@test.com', password='test_password'))
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(CustomUser.objects.count(), 1)
         self.assertEqual(CustomUser.objects.get().email, 'test@test.com')
 
 
 class OrderTest(APITestCase):
-    def create_order(self):
-        c = Client()
-        url = reverse('order-list')
-        data = {
-            'ordered_items': [],
-        }
-        CustomUser.objects.create_user(
-            email='test@test.com', password='test_password'
-        )
-        logged_in = c.login(username='test@test.com', password='test_password')
-        print(logged_in)
-        response = c.post(url, data, format='json')
-        return response
 
     def test_create_and_retrieve_order(self):
         '''
         test Order creation and retrieve
         '''
-        response = self.create_order()
+        url = reverse('order-list')
+        data = {
+            'ordered_items': [],
+        }
+
+        self.client.post(user_url, user_data, format='json')
+        self.client.login(email='test@test.com', password='test_password')
+        response = self.client.post(url, data, format='json')
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
         self.assertEqual(Order.objects.count(), 1)
